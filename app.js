8187aa4388c03fb31155cd7d9b6424b60062e1cd
JUSTFIDEL <fidenki1429@gmail.com>
2023-01-20 17:52:55 +0100

    EJS Templating

--- app.js
+++ app.js
@@ -8,45 +8,41 @@ app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({extended: true}))
 app.use(express.static('public'))
 
-let country = '';
-let temp = '';
-let desc = '';
-let icon = '';
-let imgURL = '';
 
-    let appId = 'd73af229da4765abb090ad0caff333fa'
-    let unit = 'metric';
-    let url = 'https://api.openweathermap.org/data/2.5/weather?q=' + country + '&appid=' + appId + '&units=' + unit; 
+
     
 app.get('/', (req, res) => {
     
-    res.render('list', {
-        cityName: country,
-        weatherTemp: temp,
-        weatherDesc: desc,
-        weatherIcon: icon,
-        imgUrl: imgURL
-
-    })
+    res.render('list')
 })
 
 app.post('/', (req, res) => {
 
-     country = req.body.cityName;
-
+     const country = req.body.cityName;
+     const appId = 'd73af229da4765abb090ad0caff333fa'
+     const unit = 'metric';
+     const url = 'https://api.openweathermap.org/data/2.5/weather?q=' + country + '&appid=' + appId + '&units=' + unit; 
+     
 
     
     https.get(url, (response) => {
         // console.log(response.statusCode);
 
         response.on('data', (data) => {
-             weatherData = JSON.parse(data)
-             temp = weatherData.main.temp
-             desc = weatherData.weather[0].description
-             icon = weatherData.weather[0].icon
-             imgURL = 'http://openweathermap.org/img/wn/' + icon + '@4x.png'
-
+            const weatherData = JSON.parse(data)
+            const temp = weatherData.main.temp
+            const desc = weatherData.weather[0].description
+            const icon = weatherData.weather[0].icon
+            const imgURL = 'http://openweathermap.org/img/wn/' + icon + '@4x.png'
             
+            res.render('weather', {
+                cityName: country,
+                weatherTemp: temp,
+                weatherDesc: desc,
+                weatherIcon: icon,
+                imgUrl: imgURL
+        
+            })
         })
     })
 
