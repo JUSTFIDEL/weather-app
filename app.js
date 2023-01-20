4851e95f29cdd33701acd3b4db4c22a37dd925b2
JUSTFIDEL <fidenki1429@gmail.com>
2023-01-20 13:54:45 +0100

    CSS update

--- app.js
+++ app.js
@@ -4,42 +4,58 @@ const bodyParser = require('body-parser')
 
 const app = express()
 
+app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({extended: true}))
 app.use(express.static('public'))
 
+let country = '';
+let temp = '';
+let desc = '';
+let icon = '';
+let imgURL = '';
 
+    let appId = 'd73af229da4765abb090ad0caff333fa'
+    let unit = 'metric';
+    let url = 'https://api.openweathermap.org/data/2.5/weather?q=' + country + '&appid=' + appId + '&units=' + unit; 
+    
 app.get('/', (req, res) => {
     
-    res.sendFile(__dirname + '/index.html')
+    res.render('list', {
+        cityName: country,
+        weatherTemp: temp,
+        weatherDesc: desc,
+        weatherIcon: icon,
+        imgUrl: imgURL
+
+    })
 })
 
 app.post('/', (req, res) => {
 
-    const country = req.body.cityName;
-    const appId = 'd73af229da4765abb090ad0caff333fa'
-    const unit = 'metric';
-    const url = 'https://api.openweathermap.org/data/2.5/weather?q=' + country + '&appid=' + appId + '&units=' + unit; 
+     country = req.body.cityName;
+
+
     
     https.get(url, (response) => {
         // console.log(response.statusCode);
 
         response.on('data', (data) => {
-            const weatherData = JSON.parse(data)
-            const temp = weatherData.main.temp
-            const desc = weatherData.weather[0].description
-            const icon = weatherData.weather[0].icon
-            const imgURL = 'http://openweathermap.org/img/wn/' + icon + '@4x.png'
+             weatherData = JSON.parse(data)
+             temp = weatherData.main.temp
+             desc = weatherData.weather[0].description
+             icon = weatherData.weather[0].icon
+             imgURL = 'http://openweathermap.org/img/wn/' + icon + '@4x.png'
 
-            res.write("<p>The weather is currently " + desc + "</p>");
-            res.write("<h1>The Temparature in " + country + " is " + temp + " degrees Celsius </h1>");
-            res.write("<img src=" + imgURL + ">")
-            res.send()
             
         })
     })
 
 })
 
+
+app.get('/weather', (req, res) => {
+    res.render('/weather')
+})
 
 
 
